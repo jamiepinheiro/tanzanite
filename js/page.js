aa6cc5b45b2655ad3aa04cd88185e13e9fceb9ea
Jamie Pinheiro <jamiepinheiro@gmail.com>
2018-11-02 20:09:53 -0400

    Link page together

---
+++ js/page.js
@@ -0,0 +1,8 @@
+$("a").on("click", function(e){
+	console.log("hello");
+	
+    e.preventDefault();
+    var targetID = $(this).attr('href')
+    var elementPosition = $(targetID).offset().top
+    $('html,body').animate({scrollTop: elementPosition},'slow');
+}); 
